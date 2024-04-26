c2b23fe9f2848db9a078c78f85f712d846c46ece
killij <51908793+killij@users.noreply.github.com>
2024-04-26 13:22:00 +0100

    Update OpenApiSchemaUtils.cs

--- src/TalentConsulting.TalentSuite.RisksApi/TalentConsulting.TalentSuite.RisksApi/OpenApiSchemaUtils.cs
+++ src/TalentConsulting.TalentSuite.RisksApi/TalentConsulting.TalentSuite.RisksApi/OpenApiSchemaUtils.cs
@@ -2,10 +2,25 @@
 
 internal class OpenApiSchemaUtils
 {
+    private static Dictionary<string, string> _cache = new Dictionary<string, string>();
+
     internal static string RenameDtoStrategy(Type currentClass)
     {
-        return (currentClass.Name.EndsWith("dto", StringComparison.InvariantCultureIgnoreCase) && currentClass.Name.Length > 3)
+        if (_cache.ContainsKey(currentClass.Name))
+        {
+            return _cache[currentClass.Name];
+        }
+
+        var truncate = (currentClass.FullName?.Contains(".dtos.", StringComparison.InvariantCultureIgnoreCase) ?? false) &&
+            currentClass.Name.EndsWith("dto", StringComparison.InvariantCultureIgnoreCase) &&
+            currentClass.Name.Length > 3;
+
+        var displayName = truncate
             ? currentClass.Name[..^3]
             : currentClass.Name;
+
+        _cache.Add(currentClass.Name, displayName);
+
+        return displayName;
     }
 }
